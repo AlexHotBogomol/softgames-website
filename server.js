2170acbe9e8db7b8927ffe5744d2c0c872d8fe12
AlexHotBogomol <Zakablukov777@gmail.com>
2019-12-19 13:16:05 +0200

    change port

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.prepare().then(() => {
       });
   });
 
-  server.listen(5000, err => {
+  server.listen( process.env.PORT || 5000, err => {
     if (err) throw err;
     console.log("> Read on http://localhost:5000");
   });
