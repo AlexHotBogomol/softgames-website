4511d3c42574d6b587429a7ac3b2ec2d1ab85acd
AlexHotBogomol <Zakablukov777@gmail.com>
2020-02-26 13:52:30 +0200

    add cookiebot

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,7 @@ class MyApp extends App {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
           <meta name="msapplication-TileColor" content="#da532c"/>
           <meta name="theme-color" content="#ffffff"/>
+          <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="9de0a85a-28ea-4c8d-b906-accb612b481a" data-blockingmode="auto" type="text/javascript"/>
         </Head>
         <DefaultSeo {...SEO} />
         <ModalContext.Provider
